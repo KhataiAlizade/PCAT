d449eef8002393879a6af01ca959922eb49c2f42
Xetai05 <qaqaw4930@gmail.com>
2023-11-21 21:25:55 +0100

    Workspace created

--- app.js
+++ app.js
@@ -1,3 +1,28 @@
-const fn = function () {
-    console.log("function working")
+const express = require("express")
+const path = require("path")
+const app = express();
+
+const myLogger =(req,res,next) => {
+    console.log("Middlewware Log 1")
+    next();
 }
+
+const myLogger2 =(req,res,next) => {
+    console.log("Middlewware Log 2")
+    next();
+}
+
+app.use(express.static('public'))
+app.use(myLogger)
+app.use(myLogger2)
+
+
+app.get("/", (req, res) => {
+
+    res.sendFile(path.resolve(__dirname, 'temp/index.html'))
+})
+
+const port = 3000;
+app.listen(port, () => {
+    console.log(`Sunucu ${port} portunda baslatildi.`)
+})
