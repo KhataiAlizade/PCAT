1c8ac42ead870bc8c5d3835498d3c7096241e08b
Xetai05 <qaqaw4930@gmail.com>
2023-12-03 18:21:42 +0100

    Ready for Deployment

--- controllers/photocontrollers.js
+++ controllers/photocontrollers.js
@@ -5,7 +5,7 @@ const fs = require('fs')
 exports.getAllPhotos = async (req, res) => {
 
     const page = req.query.page || 1;
-    const photosPerPage = 1;
+    const photosPerPage = 2;
     
     const totalPhotos = await Photo.find().countDocuments({}, {maxTimeMS: 60000});
     const photos = await Photo.find({})
